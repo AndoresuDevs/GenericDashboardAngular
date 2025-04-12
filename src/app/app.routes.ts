fd50ec148c5b4c03abb8825206316138f8da9233
Andres Dimas <andresdmtz@hotmail.com>
2025-04-11 22:07:21 -0600

    Initial components and define routes structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'inventory',
+        loadComponent: () => import('./features/inventory/inventory.component').then(m => m.InventoryComponent)
+    },
+    {
+        path: 'sales',
+        loadComponent: () => import('./features/sales/sales.component').then(m => m.SalesComponent)
+    },
+    {
+        path: 'management',
+        loadComponent: () => import('./features/management/management.component').then(m => m.ManagementComponent),
+        children: [
+            {
+                path: '',
+                redirectTo: 'users',
+                pathMatch: 'full',
+            },
+            {
+                path: 'users',
+                loadComponent: () => import('./features/management/users/users.component').then(m => m.UsersComponent)
+            },
+            {
+                path: 'roles',
+                loadComponent: () => import('./features/management/roles/roles.component').then(m => m.RolesComponent)
+            },
+            {
+                path: 'categories',
+                loadComponent: () => import('./features/management/categories/categories.component').then(m => m.CategoriesComponent)
+            }
+        ]
+    }
+];
