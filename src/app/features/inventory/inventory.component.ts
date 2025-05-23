16ad874be9b03ebcccb97cd5eeaf39572c5aee3c
Andres Dimas <andresdmtz@hotmail.com>
2025-05-22 19:25:26 -0600

    Table Inventory Examnple

--- src/app/features/inventory/inventory.component.ts
+++ src/app/features/inventory/inventory.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-inventory',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    DropdownModule,
+    ButtonModule
+  ],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.scss'
 })
 export class InventoryComponent {
+  search = '';
+  statusFilter: string | null = null;
 
+  items = [
+    { nombre: 'Juan Pérez', estado: 'Activo' },
+    { nombre: 'María López', estado: 'Inactivo' }
+  ];
+
+  estados = ['Activo', 'Inactivo'];
 }
