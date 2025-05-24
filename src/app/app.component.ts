117ab7a7c085efd86815ac0caef99a2ee9433cb5
Andres Dimas <andresdmtz@hotmail.com>
2025-05-23 22:45:11 -0600

    Change dinamical title and icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { FontService } from './core/services/font/font.service';
+import { Title } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +15,13 @@ import { FontService } from './core/services/font/font.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  constructor(private fontService: FontService) {}
+  constructor(
+    private fontService: FontService,
+    private titleService: Title
+  ) {}
 
   ngOnInit(): void {
     this.fontService.init();
+    this.titleService.setTitle(environment.projectName);
   }
 }
